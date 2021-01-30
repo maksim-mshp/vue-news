3fad7eb4b0ed0009ccba74bc514f6d71f3bd0d40
maksim-mshp <maksim-mshp@ya.ru>
2021-01-30 17:31:33 +0300

    Добавлены страны

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<v-app>
+		<v-app-bar app color="orange darken-1" dark>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+			<v-toolbar-title>Новости</v-toolbar-title>
+			<template v-slot:extension>
+				<v-tabs align-with-title>
+					<v-tab to="/ru">Россия</v-tab>
+					<v-tab to="/ua">Украина</v-tab>
+					<v-tab to="/usa">США</v-tab>
+					<v-tab to="/gb">Великобритания</v-tab>
+				</v-tabs>
+			</template>
+		</v-app-bar>
+
+		<v-main>
+			<router-view/>
+		</v-main>
+	</v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+	name: "App",
+
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
---
+++ src/views/UA.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        UA
+    </div>
+</template>
